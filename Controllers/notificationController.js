47d8aeca07396c3709bffbc6768e105200ab4c28
ozaa <mozaaurelia2008@gmail.com>
2026-05-30 16:45:53 +0700

    baru

--- Controllers/notificationController.js
+++ Controllers/notificationController.js
@@ -1,30 +1,30 @@
-    import connection from "../Config/database.js";
+//     import connection from "../Config/database.js";
 
-// =========================
-// GET NOTIFICATIONS USER
-// =========================
-export const getNotifications = async (req, res) => {
-  try {
-    const userId = req.user.id;
+// // =========================
+// // GET NOTIFICATIONS USER
+// // =========================
+// export const getNotifications = async (req, res) => {
+//   try {
+//     const userId = req.user.id;
 
-    const [rows] = await connection.query(
-      `
-      SELECT *
-      FROM notifications
-      WHERE user_id = ?
-      ORDER BY created_at DESC
-      `,
-      [userId]
-    );
+//     const [rows] = await connection.query(
+//       `
+//       SELECT *
+//       FROM notifications
+//       WHERE user_id = ?
+//       ORDER BY created_at DESC
+//       `,
+//       [userId]
+//     );
 
-    return res.status(200).json(rows);
+//     return res.status(200).json(rows);
 
-  } catch (error) {
-    console.error("GET NOTIF ERROR:", error);
+//   } catch (error) {
+//     console.error("GET NOTIF ERROR:", error);
 
-    return res.status(500).json({
-      message: "Failed to fetch notifications",
-      error: error.message,
-    });
-  }
-};
+//     return res.status(500).json({
+//       message: "Failed to fetch notifications",
+//       error: error.message,
+//     });
+//   }
+// };
